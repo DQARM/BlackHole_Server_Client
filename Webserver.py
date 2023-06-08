b6688b2d056231f4df137027b80c9466368c17a9
DQARM <qmawmaw@gmail.com>
2023-06-08 19:23:32 +0800

    Add files via upload

---
+++ Webserver.py
@@ -0,0 +1,21 @@
+from flask import Flask, Response, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/data')
+def data_file():
+    size = request.args.get('size', default = 1, type = int) # size in MB
+    size *= 1024 * 1024 # convert to bytes
+
+    def generate():
+        data_size = 1024 # 1 KB
+        for _ in range(size // data_size):
+            yield os.urandom(data_size)
+
+    response = Response(generate(), mimetype='application/octet-stream')
+    response.headers.set('Content-Disposition', 'attachment', filename='data')
+    return response
+
+if __name__ == '__main__':
+    app.run(port=8000)
